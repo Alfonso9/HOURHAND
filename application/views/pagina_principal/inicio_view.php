2d349cf409bfafb585511d8fe40f75edb7aa8ac4
Alfonso9 <lorenzo_5_10@hotmail.com>
2015-05-19 18:08:09 -0500

    Agregada página de inicio

---
+++ application/views/pagina_principal/inicio_view.php
@@ -0,0 +1,48 @@
+<div class="container container-fluid">
+    <div class="row">
+        <div class="col-lg-6 ">
+        	<img class="imgcareras" src="recursos/images/carreras.jpg" class="img-responsive">
+        </div>
+        <div class="col-lg-6 grid6 colorbloque">
+        	<a href="Carreras()"><h1>CARRERAS</h1></a>
+        </div>
+    </div>
+    
+    <div class="row">
+    	<div class="col-lg-4 grid4 colorbloque">
+    		<a href="verMisConv()"><h1>MATERIAS</h1></a>
+    	</div>
+    	<div class="col-lg-4">
+    		<img class="imgmaestros" src="recursos/images/maestros.jpg" class="img-responsive">
+    	</div>
+    	<div class="col-lg-4 grid4 colorbloque">
+    		<a href="Aulas()"><h1>AULAS</h1></a>
+    	</div>
+    </div>
+    <div class="row">
+    	<div class="col-lg-4">
+    		<img class="imgmaterias" src="recursos/images/materias.jpg" class="img-responsive">
+    	</div>
+    	<div class="col-lg-4 grid4 colorbloque">
+    		<a href="Maestros()"><h1>MAESTROS</h1></a>
+    	</div>
+    	<div class="col-lg-4">
+    		<img class="imgaulas" src="recursos/images/aulas.jpg" class="img-responsive">
+    	</div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-3">
+            <img class="imghorarios" src="recursos/images/horarios.jpg" class="img-responsive">
+        </div>
+        <div class="col-lg-3 grid3 colorbloque">
+            <a href="getPaginaHorario()"><h1>HORARIOS</h1></a>
+        </div>
+        <div class="col-lg-3">
+            <img class="imgreportes" src="recursos/images/reportes.jpg" class="img-responsive">
+        </div>
+        <div class="col-lg-3 grid3 colorbloque">
+            <a href=""><h1>REPORTES</h1></a>
+        </div>
+    </div>
+</div>
