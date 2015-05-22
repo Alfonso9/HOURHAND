0f6837fe0e28b5108a970bd07e7639f5caaaec59
Alfonso9 <lorenzo_5_10@hotmail.com>
2015-05-22 09:55:58 -0500

    Ligas de pagina de inicio_view.php

--- application/views/pagina_principal/inicio_view.php
+++ application/views/pagina_principal/inicio_view.php
@@ -4,19 +4,19 @@
         	<img class="imgcareras" src="recursos/images/carreras.jpg" class="img-responsive">
         </div>
         <div class="col-lg-6 grid6 colorbloque">
-        	<a onclick="Carreras()"><h1>CARRERAS</h1></a>
+        	<a onclick="carrerasPrinc()"><h1>CARRERAS</h1></a>
         </div>
     </div>
     
     <div class="row">
     	<div class="col-lg-4 grid4 colorbloque">
-    		<a href="EE()"><h1>MATERIAS</h1></a>
+    		<a href="EEPrinc()"><h1>MATERIAS</h1></a>
     	</div>
     	<div class="col-lg-4">
     		<img class="imgmaestros" src="recursos/images/maestros.jpg" class="img-responsive">
     	</div>
     	<div class="col-lg-4 grid4 colorbloque">
-    		<a onclick="Aulas()"><h1>AULAS</h1></a>
+    		<a onclick="AulasPrinc()"><h1>AULAS</h1></a>
     	</div>
     </div>
     <div class="row">
@@ -24,7 +24,7 @@
     		<img class="imgmaterias" src="recursos/images/materias.jpg" class="img-responsive">
     	</div>
     	<div class="col-lg-4 grid4 colorbloque">
-    		<a onclick="Maestros()"><h1>MAESTROS</h1></a>
+    		<a onclick="MaestrosPrinc()"><h1>MAESTROS</h1></a>
     	</div>
     	<div class="col-lg-4">
     		<img class="imgaulas" src="recursos/images/aulas.jpg" class="img-responsive">
@@ -36,13 +36,13 @@
             <img class="imghorarios" src="recursos/images/horarios.jpg" class="img-responsive">
         </div>
         <div class="col-lg-3 grid3 colorbloque">
-            <a onclick="getPaginaHorario()"><h1>HORARIOS</h1></a>
+            <a onclick="PaginaHorarioPrinc()"><h1>HORARIOS</h1></a>
         </div>
         <div class="col-lg-3">
             <img class="imgreportes" src="recursos/images/reportes.jpg" class="img-responsive">
         </div>
         <div class="col-lg-3 grid3 colorbloque">
-            <a onclick="#"><h1>REPORTES</h1></a>
+            <a onclick="ReportesPrinc()"><h1>REPORTES</h1></a>
         </div>
     </div>
 </div>
